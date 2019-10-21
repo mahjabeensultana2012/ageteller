cf99f9284187dfefe5659a567ca6f1fe7db41154
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-10-21 11:55:58 -0500

    implement stats

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,26 @@ import { Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: '',
+    };
+  }
+
+  changeBirthday() {
+    console.log(this.state);
+  }
   render() {
     return (
       <div className="App">
         <Form inline>
           <h2>Input Your Birthday</h2>
-          <FormControl type="date"></FormControl> <Button>Submit</Button>
+          <FormControl
+            type="date"
+            onChange={event => this.setState({ newDate: event.target.value })}
+          ></FormControl>{' '}
+          <Button onClick={() => this.changeBirthday()}>Submit</Button>
         </Form>
       </div>
     );
